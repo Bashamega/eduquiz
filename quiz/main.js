711cd38e0fc0b003be0d1777a885a752e057d5d5
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-23 13:55:50 +0200

    Add files via upload

---
+++ quiz/main.js
@@ -0,0 +1,18 @@
+const quiz_id = decodeURI(window.location.href.split("?").pop())
+alert(quiz_id)
+fetch("../data/quiz/tiles.json").then(res=>res.json()).then(data=>{
+    if (Array.isArray(data) && data.length > 0) {
+        titles = data.map(tile=>{
+          console.log(data)
+          const title_ = tile.name
+          console.log(title_)
+          return tile;
+        });
+        console.log(titles)
+        titles.forEach(title => {
+          if (title.name == quiz_id){
+            document.getElementById("title").innerHTML = quiz_id
+          }
+        });
+      }
+})
---
+++ script.js
@@ -0,0 +1,63 @@
+let titles = [];
+document.addEventListener('DOMContentLoaded', function() {
+  const inputElement = document.getElementById('search');
+
+  inputElement.addEventListener('input', function() {
+
+    if (document.getElementById("drop")){
+      document.querySelector('nav').removeChild(document.getElementById('drop'))
+    }
+    const val = inputElement.value
+    if (val ==""){
+      throw Error
+    }
+    console.log(val);
+    const drop = document.createElement("div")
+    drop.id="drop"
+
+    document.querySelector('nav').append(drop)
+    let mydata;
+    fetch("data/quiz/tiles.json")
+      .then(res => res.json())
+      .then(data=>{
+        if (Array.isArray(data) && data.length > 0) {
+          titles = data.map(tile=>{
+            console.log(data)
+            const title_ = tile.name
+            console.log(title_)
+            return tile;
+          });
+          console.log(titles)
+          const value = val.toLowerCase()
+          titles.forEach(title => {
+            if (title.name.toLowerCase().includes(value)){
+              const tile = document.createElement("div")
+              tile.innerHTML = `<a href = "quiz?${title.name}"<heading>${title.name}</heading><a>`
+              console.log(tile)
+              document.getElementById('drop').append(tile)
+            }
+          });
+        }
+      });
+  });
+});
+fetch("data/quiz/tiles.json")
+  .then(res=> res.json())
+  .then(data =>{
+    if (Array.isArray(data) && data.length > 0) {
+      titles = data.map(tile=>{
+        console.log(data)
+        const title_ = tile.name
+        console.log(title_)
+        return tile;
+      });
+      console.log(titles)
+      titles.forEach(title => {
+        const div = document.createElement("div")
+        div.id = "tile"
+        div.innerHTML = `<a href="quiz?${title.name}"<heading>${title.name}</heading><br><p>Tags: ${title.tags}</p><a>`
+        document.getElementById('container').append(div)
+        
+      });
+    }
+  })
