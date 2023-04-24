2ba3c6e7a7e01feae71c3be864db27706f5ffe04
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-24 10:54:44 +0200

    H

--- quiz/main.js
+++ quiz/main.js
@@ -10,28 +10,30 @@ fetch("../data/quiz/tiles.json")
         const sets = quiz.term;
         for (const question_term of quiz.term) {
           const question_div = document.createElement("div");
-          question_div.innerHTML = `<p>${question_term.question}</p><br><input type='text' name="${question_term.answer}">`;
+          question_div.innerHTML = `<p>${question_term.question}</p><br><input type='text' name="q_${question_term.id}">`;
           document.getElementById("options").append(question_div);
         }
       
       }
     }
   });
-//answer
+
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("submit").addEventListener('click', ()=>{
-    let score ;
-    let correct;
-    let error;
+    let score = 0;
+    let correct = 0;
+    let error = 0;
     const input = document.querySelectorAll('input')
     for (var i = 0; i < input.length; i++){
-      if (input[i].value.toLowerCase() == input[i].name.toLowerCase()){
-        score + 1
-        correct +1
+      const answer = input[i].name.replace('q_', '');
+      if (input[i].value.toLowerCase() == answer.toLowerCase()){
+        score++;
+        correct++;
       }else{
-        error +1
+        error++;
       }
     }
-    localStorage.setItem('Score', String(score))
+    localStorage.setItem('Score', String(score));
+    alert(`You got ${correct} out of ${input.length} correct! Your score is ${score}.`);
   })
 })
