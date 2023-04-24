217b084e073596b52987a1e5faff45a286cce8d2
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-24 06:51:04 +0200

    Test

--- quiz/main.js
+++ quiz/main.js
@@ -9,7 +9,7 @@ fetch("../data/quiz/tiles.json")
         document.getElementById("title").innerHTML = quiz_id;
         for( question in quiz.term) {
           const question_div = document.createElement("div");
-          question_div.innerHTML = `<p>${question.question}</p><br><input type='text' id="${question.answer}">`;
+          question_div.innerHTML = `<p>${slide.term["question"]}</p><br><input type='text' id="${slide.term["answer"]}">`;
           document.getElementById("options").append(question_div);
         };
       }
