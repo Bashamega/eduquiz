5a662d994653bd7c2cb3fa5c7f65d0289d3053e8
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-24 10:39:21 +0200

    test

--- quiz/main.js
+++ quiz/main.js
@@ -10,10 +10,26 @@ fetch("../data/quiz/tiles.json")
         const sets = quiz.term;
         for (const question_term of quiz.term) {
           const question_div = document.createElement("div");
-          question_div.innerHTML = `<p>${question_term.question}</p><br><input type='text' id="${question_term.answer}">`;
+          question_div.innerHTML = `<p>${question_term.question}</p><br><input type='text' name="${question_term.answer}">`;
           document.getElementById("options").append(question_div);
         }
       
       }
     }
   });
+//answer
+document.getElementById("submit").addEventListener('click', ()=>{
+  let score ;
+  let correct;
+  let error;
+  const input = document.querySelectorAll('input')
+  for (var i = 0; i < inputs.length; i++){
+    if (input[i].value == input[i].name){
+      score + 1
+      correct +1
+    }else{
+      error +1
+    }
+  }
+  localStorage.setItem('Score', String(score))
+})
