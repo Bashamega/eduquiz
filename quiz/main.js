deed438ab7ab1ae6c430eab1aef2f13ad825d8dd
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-24 06:40:03 +0200

    run

--- quiz/main.js
+++ quiz/main.js
@@ -1,23 +1,17 @@
 const quiz_id = decodeURI(window.location.href.split("?").pop())
-//alert(quiz_id)
-fetch("../data/quiz/tiles.json").then(res=>res.json()).then(data=>{
-    if (Array.isArray(data) && data.length > 0) {
-        titles = data.map(tile=>{
-          console.log(data)
-          const title_ = tile.name
-          console.log(title_)
-          return tile;
-        });
-        console.log(titles)
-        titles.forEach(title => {
-          if (title.name == quiz_id){
-            document.getElementById("title").innerHTML = quiz_id
-            title.term.forEach(question =>{
-              const question_div = document.createElement("div");
-              question_div.innerHTML = `<p>${question.question}</p><br><input type='text' id="${question.answer}">`
-              document.getElementById("options").append(question_div)
-          })
-          }
-        });
-      }
-})
+
+fetch("../data/quiz/tiles.json")
+  .then(res => res.json())
+  .then(data => {
+    const quiz = data.find(tile => tile.name === quiz_id)
+
+    if (quiz) {
+      document.getElementById("title").innerHTML = quiz.name
+
+      quiz.term.forEach(slide => {
+        const question_div = document.createElement("div");
+        question_div.innerHTML = `<p>${slide.question}</p><br><input type='text' id="${slide.answer}">`
+        document.getElementById("options").append(question_div)
+      })
+    }
+  })
