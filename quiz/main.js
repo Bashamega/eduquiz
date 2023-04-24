9f42057f19bdd63adf6b5b8b663c31ce2428a6ae
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-24 10:27:24 +0200

    Test

--- quiz/main.js
+++ quiz/main.js
@@ -7,11 +7,13 @@ fetch("../data/quiz/tiles.json")
       const quiz = data.find((tile) => tile.name === quiz_id);
       if (quiz) {
         document.getElementById("title").innerHTML = quiz_id;
-        for( question_term in quiz.term) {
+        const sets = quiz.term;
+        for (const question_term of quiz.term) {
           const question_div = document.createElement("div");
-          question_div.innerHTML = `<p>${question_term.set.question}</p><br><input type='text' id="${question_term.set.answer}">`;
+          question_div.innerHTML = `<p>${question_term.question}</p><br><input type='text' id="${question_term.answer}">`;
           document.getElementById("options").append(question_div);
-        };
+        }
+      
       }
     }
   });
