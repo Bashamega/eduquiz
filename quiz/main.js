80c4b6b265fd2f76b34b0fdf94108fda37df39bc
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-25 09:59:29 +0200

    try

--- quiz/main.js
+++ quiz/main.js
@@ -9,54 +9,57 @@ if (window.location.href.includes("?")){
 
 
 
-function run(){
-    const quiz_id = decodeURI(window.location.href.split("?").pop());
-    document.getElementById('title_').innerText = `Quiz - ${quiz_id}`
-    fetch("../data/quiz/tiles.json")
+function run() {
+  const quiz_id = decodeURI(window.location.href.split("?").pop());
+  document.getElementById('title_').innerText = `Quiz - ${quiz_id}`
+  fetch("../data/quiz/tiles.json")
     .then((res) => res.json())
     .then((data) => {
       if (Array.isArray(data) && data.length > 0) {
-        const sets = data.find((tile) => tile.name === quiz_id)?.term;
-        if (sets && Array.isArray(sets) && sets.length > 0) {
-          for (const question_term of sets) {
+        const quiz = data.find((tile) => tile.name === quiz_id);
+        if (quiz) {
+          const sets = quiz.term;
+          document.getElementById("options").innerHTML = "";
+          for (const question_term of quiz.term.question) {
             const question_div = document.createElement("div");
             question_div.innerHTML = `<p>${question_term.question}</p><br><input type='text' name="q_${question_term.answer}">`;
-            document.getElementById("options").append(question_div);
+            document.getElementById("options").append(question_div)
           }
-        } else {
-          window.location.href = "https://bashamega.github.io/eduquiz/";
+
         }
-      }      
+      } else {
+        window.location.href = "https://bashamega.github.io/eduquiz/"
+      }
     });
 
-    document.getElementById("submit").addEventListener('click', ()=>{
-      const input = document.querySelectorAll('input')
-      
-      let score = 0;
-      let correct = 0;
-      let error = 0;
-      for (var i = 0; i < input.length; i++){
-        const answer = input[i].name.replace('q_', '');
-        if (input[i].value.toLowerCase() == answer.toLowerCase()){
-          score++;
-          correct++;
-        }else{
-          error++;
-        }
-      }
-      localStorage.setItem('Score', String(score));
-      let question = correct + error
+  document.getElementById("submit").addEventListener('click', () => {
+    const input = document.querySelectorAll('input')
 
-      
-      document.getElementById('container').style.visibility = 'hidden'
-      const div = document.createElement('div');
-      div.id = "congrats"
-      let heading = 0;
-      
-      div.innerHTML = `<h1>Done</h1><br><h2>Score: ${correct} / ${question}</h2><br><a href="https://bashamega.github.io/eduquiz/"><button>Home</button></a>`
-      document.body.append(div)
-      
-    })
+    let score = 0;
+    let correct = 0;
+    let error = 0;
+    for (var i = 0; i < input.length; i++) {
+      const answer = input[i].name.replace('q_', '');
+      if (input[i].value.toLowerCase() == answer.toLowerCase()) {
+        score++;
+        correct++;
+      } else {
+        error++;
+      }
+    }
+    localStorage.setItem('Score', String(score));
+    let question = correct + error
+
+
+    document.getElementById('container').style.visibility = 'hidden'
+    const div = document.createElement('div');
+    div.id = "congrats"
+    let heading = 0;
+
+    div.innerHTML = `<h1>Done</h1><br><h2>Score: ${correct} / ${question}</h2><br><a href="https://bashamega.github.io/eduquiz/"><button>Home</button></a>`
+    document.body.append(div)
+
+  })
 }
 
 console.log(run);
