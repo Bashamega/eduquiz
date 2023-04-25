7bd396baf32ed84de912cad638ea0cdbaf48cb2a
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-25 18:13:13 +0200

    tags

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ fetch("data/quiz/tiles.json")
       titles.forEach(title => {
         const div = document.createElement("div")
         div.id = "tile"
-        div.innerHTML = `<a href="quiz?${title.name}"<heading>${title.name}</heading><br></a><a href="quiz?${title.tags}"><div id='tag'>${title.tags}</div></a>`
+        div.innerHTML = `<a href="quiz?${title.name}"<heading>${title.name}</heading><br></a><a href="tag?${title.tags}"><div id='tag'>${title.tags}</div></a>`
         document.getElementById('container').append(div)
         
       });
