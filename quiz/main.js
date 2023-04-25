d93641187e43d47cc4b4de89ed20738493143c70
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-25 16:28:30 +0200

    try

--- quiz/main.js
+++ quiz/main.js
@@ -7,60 +7,56 @@ if (window.location.href.includes("?")){
   window.location.href = "https://bashamega.github.io/eduquiz/"
 }
 
-function run() {
-  console.log("run")
-  const quiz_id = decodeURI(window.location.href.split("?").pop());
-  document.getElementById('title_').innerText = `Quiz - ${quiz_id}`
-  fetch("../data/quiz/tiles.json")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-      if (Array.isArray(data) && data.length > 0) {
-        const quiz = data.find((tile) => tile.name === quiz_id);
-        console.log(quiz)
-        if (quiz) {
-          console.log(true)
-          const sets = quiz.term;
-          document.getElementById("options").innerHTML = "";
-          for (const question_term of sets){
-            if(question_term == Array.isArray(sets.question)){
-              const question_div = document.createElement("div");
-              question_div.innerHTML = `<p>${question_term.question}</p><br><input type='text' name="${question_term.answer}">`;
-              document.getElementById("options").append(question_div)
-            }
-          }          
-        }
-      } else {
-        window.location.href = "https://bashamega.github.io/eduquiz/"
+console.log("run")
+const quiz_id = decodeURI(window.location.href.split("?").pop());
+document.getElementById('title_').innerText = `Quiz - ${quiz_id}`
+fetch("../data/quiz/tiles.json")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
+    if (Array.isArray(data) && data.length > 0) {
+      const quiz = data.find((tile) => tile.name === quiz_id);
+      console.log(quiz)
+      if (quiz) {
+        console.log(true)
+        const sets = quiz.term;
+        document.getElementById("options").innerHTML = "";
+        for (const question_term of sets){
+          if(question_term == Array.isArray(sets.question)){
+            const question_div = document.createElement("div");
+            question_div.innerHTML = `<p>${question_term.question}</p><br><input type='text' name="${question_term.answer}">`;
+            document.getElementById("options").append(question_div)
+          }
+        }          
       }
-    });
-
-  document.getElementById("submit").addEventListener('click', () => {
-    const input = document.querySelectorAll('input')
-
-    let score = 0;
-    let correct = 0;
-    let error = 0;
-    for (var i = 0; i < input.length; i++) {
-      const answer = input[i].name
-      if (input[i].value.toLowerCase() == answer.toLowerCase()) {
-        score++;
-        correct++;
-      } else {
-        error++;
-      }
+    } else {
+      window.location.href = "https://bashamega.github.io/eduquiz/"
     }
-    localStorage.setItem('Score', String(score));
-    let question = correct + error
+  });
 
-    document.getElementById('container').style.visibility = 'hidden'
-    const div = document.createElement('div');
-    div.id = "congrats"
-    let heading = 0;
+document.getElementById("submit").addEventListener('click', () => {
+  const input = document.querySelectorAll('input')
 
-    div.innerHTML = `<h1>Done</h1><br><h2>Score: ${correct} / ${question}</h2><br><a href="https://bashamega.github.io/eduquiz/"><button>Home</button></a>`
-    document.body.append(div)
-  })
-}
+  let score = 0;
+  let correct = 0;
+  let error = 0;
+  for (var i = 0; i < input.length; i++) {
+    const answer = input[i].name
+    if (input[i].value.toLowerCase() == answer.toLowerCase()) {
+      score++;
+      correct++;
+    } else {
+      error++;
+    }
+  }
+  localStorage.setItem('Score', String(score));
+  let question = correct + error
 
-window.onload = run;
+  document.getElementById('container').style.visibility = 'hidden'
+  const div = document.createElement('div');
+  div.id = "congrats"
+  let heading = 0;
+
+  div.innerHTML = `<h1>Done</h1><br><h2>Score: ${correct} / ${question}</h2><br><a href="https://bashamega.github.io/eduquiz/"><button>Home</button></a>`
+  document.body.append(div)
+})
