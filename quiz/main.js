f8e8616cc4a00f3f116805c78c165c7be0c72e48
Basha <110662505+Bashamega@users.noreply.github.com>
2023-04-25 17:16:54 +0200

    test

--- quiz/main.js
+++ quiz/main.js
@@ -23,13 +23,13 @@ fetch("../data/quiz/tiles.json")
         document.getElementById("options").innerHTML = "";
         for (question_term of sets){
           console.log(question_term)
-          if(question_term == Array.isArray(question_term.question)){
-            console.log(true)
+          if (Array.isArray(question_term.question)) {
+            console.log(true);
             const question_div = document.createElement("div");
             question_div.innerHTML = `<p>${question_term.question}</p><br><input type='text' name="${question_term.answer}">`;
-            document.getElementById("options").append(question_div)
-            console.log("done")
-          }
+            document.getElementById("options").append(question_div);
+            console.log("done");
+          }          
         }          
       }
     } else {
