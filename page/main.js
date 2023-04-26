f1e5bcc6ddaaeddaff5ca69897692d8cdd6e46b9
Bashamega <adambashaahmednaji@gmail.com>
2023-04-26 17:01:04 +0200

    edit

--- page/main.js
+++ page/main.js
@@ -8,32 +8,32 @@ if (window.location.href.includes("?")){
   window.location.href = "https://bashamega.github.io/eduquiz/"
 }
 
-document.getElementById('title_').innerText = `Study - ${quiz_id}`
+document.getElementById('title').innerText = `Study - ${quiz_id}`
 fetch("../data/quiz/tiles.json")
   .then((res) => res.json())
   .then((data) => {
-    let done = 0;
     if (Array.isArray(data) && data.length > 0) {
       const quiz = data.find((tile) => tile.name === quiz_id);
       if (quiz) {
         const sets = quiz.term;
-        document.getElementById("options").innerHTML = "";
-        for (question_term of sets){
-          const div = document.createElement('tr')
+        const tbody = document.getElementById("set_study");
+        tbody.innerHTML = "";
+        for (const question_term of sets){
+          const tr = document.createElement('tr')
           const td_question = document.createElement('td')
           const td_answer = document.createElement('td')
           td_question.innerText = question_term.question;
           td_answer.innerText = question_term.answer;
-          div.id="term"
 
-          document.getElementById('set_study').append(div)
-          document.getElementById('term').append(td_question)
-          document.getElementById('term').append(td_answer)
+          tr.appendChild(td_question);
+          tr.appendChild(td_answer);
+          tbody.appendChild(tr);
         }
-        let +1
+      }
+      else {
+        window.location.href = "https://bashamega.github.io/eduquiz/"
       }
     } else {
       window.location.href = "https://bashamega.github.io/eduquiz/"
     }
-    if(done == 0){window.location.href = "https://bashamega.github.io/eduquiz/"}
   });
