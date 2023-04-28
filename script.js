312b508e7a484cc47a62b8fe13ab971961669152
Bashamega <adambashaahmednaji@gmail.com>
2023-04-28 14:09:25 +0300

    push

--- script.js
+++ script.js
@@ -54,11 +54,12 @@ fetch("https://bashamega.github.io/eduquiz/data/quiz/tiles.json")
       });
       titles.forEach((title) => {
         const div = document.createElement("div");
-        div.id = "tile";
+        
         div.classList.add(title.tags);
+        div.classList.add('tile')
 
         const name_const = encodeURI(title.name);
-        
+        div.id = name_const;
         div.innerHTML = `<heading>${title.name}</heading><br><br><p>Tags:   </p><a href="tag?${title.tags}"><button id='tag'>${title.tags}</button><Br><br></a><div class="choice"><a href="page?${title.name}"><button>Study</button></a><a href="quiz?${title.name}"><button >Quiz</button></a></div><div id='points' onclick="sidemenu('${name_const}')" class="${name_const}"><span>o</span ><span>0</span><span>o</span></div>`;
         
         document.getElementById("container").append(div);
@@ -79,10 +80,41 @@ function sidemenu(string) {
  
   const div =  document.createElement('div')
   div.id = 'menu'
-  let content =`<h3>${name_decoded}</h3><br><a href="https://bashamega.github.io/eduquiz/quiz?${string}<button>Open</button></a><button onclick=share(${string})>Share<button>`
+  let content = `<a href="https://bashamega.github.io/eduquiz/quiz?${string}"><button>Open</button></a><button onclick="share('${string}')">Share</button>`;
   div.innerHTML= content
-  alert(content)
   document.getElementsByClassName(string)[0].appendChild(div);
 
   
 }
+function share(string){
+  const div = document.createElement('div');
+  div.id = "social";
+  const input = document.createElement('input');
+  input.readOnly = true;
+  input.value = `https://bashamega.github.io/eduquiz/#${string}`;
+  document.body.append(div);
+  document.getElementById('social').appendChild(input);
+  
+
+  const btn = document.createElement('button')
+  btn.innerText ='close'
+  document.getElementById('social').appendChild(btn)
+  btn.addEventListener('click', function(){
+    document.getElementById('social').parentNode.removeChild(document.getElementById('social'))
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  if(window.location.href.includes("#")){
+    const id = window.location.href.split('#').pop();
+    const element = document.getElementById(id);
+    if (element) {
+      element.classList.add('hover');
+      
+    }
+  }
+  if(document.getElementById('social')){
+    document.getElementById('social').parentNode.removeChild(document.getElementById('social'))
+
+  }
+});
