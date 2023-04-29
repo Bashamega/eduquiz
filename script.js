a58f044659f550d2e26c9ef2089f96ffc5a7328b
Bashamega <adambashaahmednaji@gmail.com>
2023-04-29 08:52:16 +0300

    style

--- script.js
+++ script.js
@@ -59,12 +59,23 @@ fetch("https://bashamega.github.io/eduquiz/data/quiz/tiles.json")
         div.classList.add('tile')
 
         const name_const = encodeURI(title.name);
-        div.id = name_const;
+        div.id = name_const.replace('%20', '_');
         div.innerHTML = `<heading>${title.name}</heading><br><br><p>Tags:   </p><a href="tag?${title.tags}"><button id='tag'>${title.tags}</button><Br><br></a><div class="choice"><a href="page?${title.name}"><button>Study</button></a><a href="quiz?${title.name}"><button >Quiz</button></a></div><div id='points' onclick="sidemenu('${name_const}')" class="${name_const}"><span>o</span ><span>0</span><span>o</span></div>`;
         
         document.getElementById("container").append(div);
+        div.addEventListener('mouseover', function(){
+          div.classList.add('visible')
+          
+        })
+        div.addEventListener('mouseout', function(){
+          div.classList.remove('visible')
+        })
+
+
+
       });
     }
+    check()
   })
   .catch((err) => {
     console.log(err);
@@ -91,7 +102,7 @@ function share(string){
   div.id = "social";
   const input = document.createElement('input');
   input.readOnly = true;
-  input.value = `https://bashamega.github.io/eduquiz/#${string}`;
+  input.value = `https://bashamega.github.io/eduquiz/?${string}`;
   document.body.append(div);
   document.getElementById('social').appendChild(input);
   
@@ -103,13 +114,20 @@ function share(string){
     document.getElementById('social').parentNode.removeChild(document.getElementById('social'))
   })
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  if(window.location.href.includes("#")){
-    const id = window.location.href.split('#').pop();
-    const element = document.getElementById(id);
+function check(){
+  if(window.location.href.includes("?")){
+    const id_ = window.location.href.split('?').pop().replace('%20', '_');
+    const element = document.getElementById(id_);
+    
     if (element) {
+
       element.classList.add('hover');
+      const tile = document.getElementsByClassName('tile')
+      for( i in tile){
+        tile[i].addEventListener('mouseover', function(){
+          element.classList.remove('hover')
+        })
+      }
       
     }
   }
@@ -117,4 +135,5 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('social').parentNode.removeChild(document.getElementById('social'))
 
   }
-});
+}
+
